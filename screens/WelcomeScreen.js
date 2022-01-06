95ef56c52943b832a5c63d969b0baced637817d8
sarojkafle30369670 <64567309+sarojkafle30369670@users.noreply.github.com>
2022-01-06 12:42:44 +1100

    add existing file

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,111 +1,176 @@
-import { db, auth } from '../firebase';
-import React, { useLayoutEffect }from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import { Avatar,Button } from 'react-native-elements';
-import { Entypo } from '@expo/vector-icons';
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Button, Avatar } from "react-native-elements";
+import { Entypo } from "@expo/vector-icons";
+import { auth, db } from "../firebase";
 
+const WelcomeScreen = ({ navigation }) => {
+  const [index, setIndex] = React.useState(0);
+  const [loading, setLoading] = useState(true); // Set loading to true on component mount
+  const [users, setUsers] = useState([]); // Initial empty array of users
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <Avatar
+            rounded
+            source={{
+              uri: auth?.currentUser?.photoURL,
+            }}
+          />
+        </View>
+      ),
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 30,
+          }}
+          onPress={signOut}
+        >
+          <Entypo name="log-out" size={24} color="black" />
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
 
+  const signOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+        navigation.replace("Login");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
 
-const WelcomeScreen = ({navigation}) => {
-  
-    
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerLeft: () => (
-                <View style={{ marginLeft: 20 }}>
-                    <Avatar
-                        rounded
-                        source={{
-                            uri: auth?.currentUser?.photoURL
-                        }}
-                    />
-                </View>
-            ),
-            headerRight: () => (
-                <TouchableOpacity style={{
-                    marginRight: 30
+  useEffect(() => {
+    const subscriber = db
+      .collection(auth.currentUser.uid)
+      .onSnapshot((querySnapshot) => {
+        const users = [];
+
+        querySnapshot.forEach((documentSnapshot) => {
+          users.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+
+        setUsers(users);
+        setLoading(false);
+      });
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <>
+      <Button
+        title="Add Contact"
+        onPress={() => navigation.navigate("Contact")}
+        style={styles.button}
+      />
+      <View
+        style={{
+          flex: 5,
+          justifyContent: "fill",
+          alignItems: "left",
+          marginTop: "2%",
+        }}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <View
+            style={{ width: 130, border: "solid line", backgroundColor: "red" }}
+          >
+            <Text style={{ color: "white" }}>Name</Text>
+          </View>
+          <View
+            style={{ width: 130, border: "solid line", backgroundColor: "red" }}
+          >
+            <Text style={{ color: "white" }}>Photo</Text>
+          </View>
+          <View
+            style={{ width: 130, border: "solid line", backgroundColor: "red" }}
+          >
+            <Text style={{ color: "white" }}>Email</Text>
+          </View>
+          <View
+            style={{ width: 130, border: "solid line", backgroundColor: "red" }}
+          >
+            <Text style={{ color: "white" }}>Phone</Text>
+          </View>
+        </View>
+        <FlatList
+          data={users}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: "row" }}>
+              <View
+                style={{
+                  width: 130,
+                  border: "solid line",
+                  backgroundColor: "yellow",
                 }}
-                    onPress={signOut}
-                >
-                    <Entypo name="log-out" size={24} color="black" />
-                </TouchableOpacity>
-            )
-        })
-
-    }, [])
-
-    const signOut = () => {
-        auth.signOut().then(() => {
-            // Sign-out successful.
-            navigation.replace('Login')
-        }).catch((error) => {
-            // An error happened.
-        });
-    }
-
-    const listData =()=>{
-        db.collection(auth.currentUser.uid).get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
-            });
-        });
-        return listData;
-    }
-
-
-    return (
-        <>
-            <View>
-                <Text>Welcome {auth?.currentUser?.displayName}</Text>
-                <Button title="Add New Contact"  onPress={()=>navigation.navigate('Contact')} style={styles.button} />
-                <Button title="List Contact" onPress={listData} style={styles.button} />
-
+              >
+                <Text>{item.name}</Text>
+              </View>
+              <View
+                style={{
+                  width: 130,
+                  border: "solid line",
+                  backgroundColor: "yellow",
+                }}
+              >
+                <Text>{item.photo}</Text>
+              </View>
+              <View
+                style={{
+                  width: 130,
+                  border: "solid line",
+                  backgroundColor: "yellow",
+                }}
+              >
+                <Text>{item.email}</Text>
+              </View>
+              <View
+                style={{
+                  width: 130,
+                  border: "solid line",
+                  backgroundColor: "yellow",
+                }}
+              >
+                <Text>{item.phone}</Text>
+              </View>
             </View>
-            {/* <DataTable>
-                <DataTable.Header>
-                <DataTable.Title>Dessert</DataTable.Title>
-                <DataTable.Title numeric>Calories</DataTable.Title>
-                <DataTable.Title numeric>Fat</DataTable.Title>
-                </DataTable.Header>
+          )}
+        />
+      </View>
+    </>
+  );
 
-                <DataTable.Row>
-                <DataTable.Cell>Frozen yogurt</DataTable.Cell>
-                <DataTable.Cell numeric>159</DataTable.Cell>
-                <DataTable.Cell numeric>6.0</DataTable.Cell>
-                </DataTable.Row>
-
-                <DataTable.Row>
-                <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-                <DataTable.Cell numeric>237</DataTable.Cell>
-                <DataTable.Cell numeric>8.0</DataTable.Cell>
-                </DataTable.Row>
-
-                <DataTable.Pagination
-                page={page}
-                numberOfPages={3}
-                onPageChange={(page) => setPage(page)}
-                label="1-2 of 6"
-                optionsPerPage={optionsPerPage}
-                itemsPerPage={itemsPerPage}
-                setItemsPerPage={setItemsPerPage}
-                showFastPagination
-                optionsLabel={'Rows per page'}
-                />
-            </DataTable> */}
-
-        </>
-    )
-}
-
-export default WelcomeScreen
+  // ...
+};
+export default WelcomeScreen;
 const styles = StyleSheet.create({
-    button: {
-        width: 200,
-        marginTop: 10
-    },
-    container: {
-        padding: 10
-    }
-})
-
+  button: {
+    width: 150,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  container: {
+    padding: 10,
+  },
+});
